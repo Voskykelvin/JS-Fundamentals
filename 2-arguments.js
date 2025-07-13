f4cfa8eda64eec5217641329d479500c6b600e7f
Kelvin Ochieng <kelvinvosky2@gmail.com>
2025-07-13 18:48:56 +0300

    Initial commit

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+let argument = 0;
+if(argument == 0)
+    {
+    console.log("No argument");
+}
+else if(argument == 1){
+
+    console.log("Argument found");
+}
+else{
+    console.log("Arguments found");
+}
